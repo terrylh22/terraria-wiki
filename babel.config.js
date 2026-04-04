bc271532e1d78b8453fc32f781604ed2b640f4f9
terrylh22 <63936327+terrylh22@users.noreply.github.com>
2026-04-03 21:07:04 -0400

    Initial scaffolding of the app

--- babel.config.js
+++ babel.config.js
@@ -4,6 +4,8 @@ module.exports = function (api) {
     presets: [
       ['babel-preset-expo', { jsxImportSource: 'nativewind' }],
     ],
-    plugins: ['nativewind/babel'],
+    plugins: [
+      'react-native-reanimated/plugin',
+    ],
   };
 };
